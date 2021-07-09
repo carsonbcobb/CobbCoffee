498d9cbbf1b8eb937c49ab156ace637b1472ee15
carsonbcobb <carsonbcobb@gmail.com>
2021-07-09 05:53:29 -0700

    desktop and mobile

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,13 @@
 // Put your application javascript here
+
+$(document).ready(function () {
+	$('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		arrows: false,
+		dots: true,
+		infinite: true,
+	});
+});
